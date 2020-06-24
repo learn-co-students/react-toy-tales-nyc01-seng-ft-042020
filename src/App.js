85b27d733824e34946d21e9e48f9d54747b2ea89
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-06-24 17:16:04 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
+// import data from './data'
 
 
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys:[]
   }
 
   handleClick = () => {
@@ -20,21 +21,75 @@ class App extends React.Component{
       display: newBoolean
     })
   }
-
+  componentDidMount=()=>{
+    fetch('http://localhost:3000/toys')
+    .then(r=>r.json())
+    .then(data=>this.setState({toys:data}))
+  }
+  addNewToy=(toy)=>{
+    fetch('http://localhost:3000/toys',{
+      method:'POST',
+      headers:{
+        "content-type":"application/json",
+        accept:"application/json"
+      },
+      body:JSON.stringify(toy)
+    })
+    .then(r=>r.json())
+    .then(data=>{
+      const newToys=[...this.state.toys,data]
+      this.setState({
+        toys:newToys
+      })
+    })
+  }
+  incrementLikes=(toy)=>{
+    fetch(`http://localhost:3000/toys/${toy.id}`,{
+      method:'PATCH',
+      headers:{
+        "content-type":"application/json",
+        accept:"application/json"
+      },
+      body:JSON.stringify({
+        likes:toy.likes+1
+      })
+    }).then(r=>{
+      const newToys=this.state.toys.map(toyCheck=>{
+        const newToy={...toyCheck}
+        if(toyCheck===toy){
+          newToy.likes +=1
+        }
+        return newToy
+      })
+      this.setState({
+        toys:newToys
+      })
+    })
+  }
+  donateToy=(id)=>{
+    fetch(`http://localhost:3000/toys/${id}`,{
+      method:'DELETE',
+    }).then(r=>{
+      const newToys = this.state.toys.filter(toy=>
+        toy.id!==id
+      )
+      this.setState({toys:newToys})
+    })
+  }
   render(){
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm addNewToy={this.addNewToy}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer incrementLikes={this.incrementLikes} donateToy={this.donateToy} toys={this.state.toys}/>
       </>
     );
   }
