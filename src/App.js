8da446d718dc189828e3846c2abb9b29a79d2a2c
rwilt <rosie.wilt@gmail.com>
2020-06-24 22:45:11 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -11,31 +11,80 @@ import data from './data'
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toyList: []
   }
 
-  handleClick = () => {
+  addToy = (newToy) => {
+    let copyOfList = [...this.state.toyList, newToy]
+    this.setState({
+    toyList: copyOfList
+    })
+  }
+
+  handleClick = (newToy) => {
     let newBoolean = !this.state.display
     this.setState({
       display: newBoolean
     })
   }
 
+  updateLikedToys = (updatedToy) => {
+    let copyOfList = this.state.toyList.map((toy) => {
+    if(toy.id === updatedToy.id){
+      return updatedToy
+    } else {
+      return toy
+    }
+  })
+
+    this.setState({
+      toyList: copyOfList
+    });
+
+}
+
+deleteToy = (passedInToyId)=> {
+let copyOfList = this.state.toyList.filter((toy)=>{
+  return toy.id !== passedInToyId
+})
+}
+
+  //use component did mount to fetch and render component
+componentDidMount(){
+  let url = "http://localhost:3000/toys"
+  fetch(url)
+  .then(resp => resp.json())
+  .then((toyArray)=> {
+    this.setState({
+      toyList: toyArray
+    });
+  });
+}
+
   render(){
+    const {toyList} = this.state
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm
+          addToy = {this.addToy}
+          />
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer
+        toys={toyList}
+        updateLikedToys={this.updateLikedToys}
+        deleteToy= {this.deleteToy}/>
+        
       </>
+
     );
   }
 
--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -2,15 +2,50 @@ import React, { Component } from 'react';
 
 class ToyCard extends Component {
 
+  likeHandler = (evt) => {
+    let numLikes = this.props.toy.likes + 1
+    fetch(`http://localhost:3000/toys/${this.props.toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type" : "application/json"
+      },
+      body: JSON.stringify({
+        likes: numLikes
+      })
+    })
+      .then(resp => resp.json())
+      .then((updatedToy)=> {
+        this.props.updateLikedToys(updatedToy)
+      })
+
+
+    }
+
+  delHandler = (evt) => {
+    fetch(`http://localhost:3000/toys/${this.props.toy.id}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+    .then(resp => resp.json())
+    .then((delToy)=>{
+      this.props.deleteToy(this.props.toy.id)
+    })
+  }
+
   render() {
+    let {name, image, likes} = this.props.toy
     return (
+     
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{name}</h2>
+        <img src={image} alt={name} className="toy-avatar" />
+        <p>{likes} Likes </p>
+        <button className="like-btn" onClick={this.likeHandler}>Like {'<3'}</button>
+        <button className="del-btn" onClick={this.delHandler}>Donate to GoodWill</button>
       </div>
+   
     );
   }
 
