267c56395bcb7f0ce5b59e524869dd27bbd114db
github username <github email address>
2020-06-25 00:19:30 -0400

    Done.

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 
 class ToyCard extends Component {
+  handleDonate = (e) => {
+    this.props.donateToy(this.props.toy.id) 
+  }
 
+  handleLike = (e) => {
+    this.props.increaseLikes(this.props.toy)
+  }
+  
   render() {
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{this.props.toy.name}</h2>
+        <img src={this.props.toy.image} alt={this.props.toy.name} className="toy-avatar" />
+        <p>{this.props.toy.likes} Likes </p>
+        <button className="like-btn" onClick={(e) => this.handleLike(e)}>Like {'<3'}</button>
+        <button className="del-btn" onClick={(e) => this.handleDonate(e)}>Donate to GoodWill</button>
       </div>
     );
   }
